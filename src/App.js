363c196f9ebad30bdf287cec404292b4ecc03964
RachelyYaari <rachely4631@gmail.com>
2024-03-06 14:07:15 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import SignUp from './components/signUp';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import LogIn from './components/logIn';
+import ShowTask from './components/showTask';
+import { Route, Routes } from 'react-router-dom';
+import ShowTasks from './components/showTasks';
+import AddTask from './components/addTask';
+import Nav from './components/nav';
+import { useState } from 'react';
 import './App.css';
+import picture from './components/picture2.jpg'
+
 
 function App() {
+  const [flagConect,setFlagConect]=useState(true);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={store}>
+        <Nav flagConect={flagConect} setFlagConect={setFlagConect}/>
+        <Routes>
+          <Route path="/SignUp" element={<SignUp flagConect={flagConect} setFlagConect={setFlagConect}/>}/>
+          <Route path="/LogIn" element={<LogIn flagConect={flagConect} setFlagConect={setFlagConect}/>}/>
+          <Route path='/ShowTask' element={<ShowTask/>}/>
+          <Route path='/ShowTasks' element={<ShowTasks flagConect={flagConect} setFlagConect={setFlagConect}/>}/>
+          <Route path='/addTask' element={<AddTask flagConect={flagConect} setFlagConect={setFlagConect}/>}/>
+          <Route path='/' element={<img src={picture} width='100%'></img>}></Route>
+        </Routes>
+      </Provider>
     </div>
   );
 }
 
 export default App;
+
+
